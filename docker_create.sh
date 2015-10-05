d69ba0aa6ffc324e1010cb7f0732225b73748ea4
Gergely Papp <gergely.papp@itworks.hu>
2015-10-05 17:41:19 +0200

    Added docker support

--- docker_create.sh
+++ docker_create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ## This snippet is used by the build script to create container specific to the project
 docker create \
-    -P -p 0.0.0.0:$SERVER_PORT:8082 \
+    -P -p 0.0.0.0:$SERVER_PORT:8081 \
     --env TYPE=${TYPE} \
     --env DB_PORT=${DB_PORT} \
     --env HTTP_PORT=${HTTP_PORT} \
