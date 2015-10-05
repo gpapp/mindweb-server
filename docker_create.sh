8306f69aca42642e6831f2ba77453f1a157fd90c
Gergely Papp <gergely.papp@itworks.hu>
2015-10-05 16:29:05 +0200

    Moved port to env

--- docker_create.sh
+++ docker_create.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 ## This snippet is used by the build script to create container specific to the project
 docker create \
-    -P -p 0.0.0.0:$HTTP:8082 \
+    -P -p 0.0.0.0:$SERVER_PORT:8082 \
     --env TYPE=${TYPE} \
+    --env DB_PORT=${DB_PORT} \
+    --env HTTP_PORT=${HTTP_PORT} \
+    --env SERVER_PORT=${SERVER_PORT} \
     --volume `pwd`/../.config/$TYPE/server:/home/node/config \
     --name mw-server-$TYPE \
     mindweb/server
