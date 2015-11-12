93cf4ac27f7d0ca7b5504a8aa8cbbf79667beb01
Papp Gergely <gergely.papp@itworks.hu>
2015-11-12 12:04:00 +0100

    Updated unit tests

---
+++ test/task/tasktest2.mm
@@ -0,0 +1,145 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="My tasks" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1427226124888"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="Calibri" SIZE="10" BOLD="false" ITALIC="false"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" BACKGROUND_COLOR="#fefc74">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="7"/>
+<node TEXT="Someday-Maybe" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_1137711775" CREATED="1427225928280" MODIFIED="1427226969704">
+<edge COLOR="#ff00ff"/>
+<node TEXT="FreeplaneWeb" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_265885849" CREATED="1427231207820" MODIFIED="1427231218466">
+<node TEXT="*Create web based mindmap editor for Freeplane with the same functionality {Someday} @Computer @Hobby" ID="ID_825572237" CREATED="1427231222501" MODIFIED="1427231281279"/>
+</node>
+<node TEXT="Freeplane GTD" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1342627143" CREATED="1427298564675" MODIFIED="1427298578517">
+<node TEXT="* Create cloud synchronization feature for Freeplane {Someday} @Computer @Hobby" ID="ID_805969038" CREATED="1427298513352" MODIFIED="1427298553304"/>
+<node TEXT="* Create Freeplane Android client" ID="ID_1460792926" CREATED="1427298638982" MODIFIED="1427298684965">
+<node TEXT="With attribute support" ID="ID_1707305919" CREATED="1427298686159" MODIFIED="1427298690372"/>
+<node TEXT="Compatible GTD view" ID="ID_672776537" CREATED="1427298690911" MODIFIED="1427298704789"/>
+</node>
+</node>
+<node TEXT="Skills" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_391031802" CREATED="1427298571259" MODIFIED="1427298577125">
+<node TEXT="* Improve presentation skills {Someday}" ID="ID_972517531" CREATED="1427298579619" MODIFIED="1427298596825"/>
+<node TEXT="* Learn to write Android applications {Someday}" ID="ID_391734240" CREATED="1427298598804" MODIFIED="1427298628338"/>
+</node>
+</node>
+<node TEXT="Archive" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="left" ID="ID_1068479685" CREATED="1427225933330" MODIFIED="1427226969057">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="Config" LOCALIZED_STYLE_REF="styles.topic" POSITION="left" ID="ID_867421423" CREATED="1427226495651" MODIFIED="1427227152051">
+<edge COLOR="#7c0000"/>
+<node TEXT="Icon: @Computer" ID="ID_1821210832" CREATED="1427226511560" MODIFIED="1427226607171">
+<icon BUILTIN="male1"/>
+</node>
+<node TEXT="Icon: @email" ID="ID_368331860" CREATED="1427226532275" MODIFIED="1427226613079">
+<icon BUILTIN="Mail"/>
+</node>
+<node TEXT="Icon: @Meeting" ID="ID_1136940334" CREATED="1427226567638" MODIFIED="1427226653217">
+<icon BUILTIN="group"/>
+</node>
+<node TEXT="Icon: @Home" ID="ID_635308207" CREATED="1427226636588" MODIFIED="1427226659054">
+<icon BUILTIN="gohome"/>
+</node>
+</node>
+<node TEXT="Inbox" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1133937836" CREATED="1427225825443" MODIFIED="1427226966224">
+<edge COLOR="#ff0000"/>
+<node TEXT="* Must create GTD presentation {03.24} [Papp Gergely] @Home,Computer" ID="ID_1916348085" CREATED="1427227326210" MODIFIED="1427231521272"><richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Outline
+    </p>
+    <p>
+      &#160;- why use mindmap
+    </p>
+    <p>
+      &#160;- how to use freeplane
+    </p>
+    <p>
+      &#160;- Live demo
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+<node TEXT="* Buy dog food @Home @Shop" ID="ID_1190980308" CREATED="1427227601160" MODIFIED="1427227649916"/>
+</node>
+<node TEXT="Projects" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_1256051703" CREATED="1427225914994" MODIFIED="1427226965326">
+<edge COLOR="#ffff00"/>
+<node TEXT="Work" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_643907318" CREATED="1427227287355" MODIFIED="1427227314713">
+<icon BUILTIN="list"/>
+<node TEXT="General" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_385574729" CREATED="1427231494328" MODIFIED="1427231709648">
+<icon BUILTIN="list"/>
+<node TEXT="* Talk to boss about a raise [Pointy Haired Boss] {4.1}" ID="ID_759486144" CREATED="1427231527097" MODIFIED="1427231573301"/>
+</node>
+<node TEXT="Process optimization project" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_914627740" CREATED="1427231501308" MODIFIED="1427231668045">
+<icon BUILTIN="list"/>
+<node TEXT="* Spend hours with HPC {4.1} [Dogbert] @Meeting" ID="ID_1900184887" CREATED="1427231669835" MODIFIED="1427231734975"/>
+<node TEXT="* Reply to consultant&apos;s QA assessment @email [Dogbert]" ID="ID_1236162794" CREATED="1427231735450" MODIFIED="1427231786427"/>
+</node>
+</node>
+<node TEXT="Meetups" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_99225657" CREATED="1427227292551" MODIFIED="1427227309084">
+<icon BUILTIN="list"/>
+</node>
+<node TEXT="Hobby" LOCALIZED_STYLE_REF="AutomaticLayout.level,2" ID="ID_1535763691" CREATED="1427231176268" MODIFIED="1427231195818">
+<icon BUILTIN="list"/>
+<node TEXT="FreeplaneGTD" LOCALIZED_STYLE_REF="AutomaticLayout.level,3" ID="ID_471511405" CREATED="1427231197233" MODIFIED="1427231288794">
+<node TEXT="* Archive competed tasks with structures {Later} [Papp Gergely] @Computer @Hobby" ID="ID_1561371231" CREATED="1427231289474" MODIFIED="1427231487180"/>
+</node>
+</node>
+</node>
+<node TEXT="Review" LOCALIZED_STYLE_REF="AutomaticLayout.level,1" POSITION="right" ID="ID_117855996" CREATED="1427225917881" MODIFIED="1427226967361">
+<edge COLOR="#00ff00"/>
+</node>
+</node>
+</map>
---
+++ test/task/tasktest3.mm
@@ -0,0 +1,77 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="My tasks" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1427226124888"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="Calibri" SIZE="10" BOLD="false" ITALIC="false"/>
+<edge STYLE="bezier" WIDTH="thin"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" BACKGROUND_COLOR="#fefc74">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8"/>
+<node TEXT="Config" LOCALIZED_STYLE_REF="styles.topic" POSITION="left" ID="ID_867421423" CREATED="1427226495651" MODIFIED="1427227152051">
+<edge COLOR="#7c0000"/>
+<node TEXT="Icon: @Computer" ID="ID_1821210832" CREATED="1427226511560" MODIFIED="1427226607171">
+<icon BUILTIN="male1"/>
+</node>
+<node TEXT="Icon: @email" ID="ID_368331860" CREATED="1427226532275" MODIFIED="1427226613079">
+<icon BUILTIN="Mail"/>
+</node>
+<node TEXT="Icon: @Meeting" ID="ID_1136940334" CREATED="1427226567638" MODIFIED="1427226653217">
+<icon BUILTIN="group"/>
+</node>
+<node TEXT="Icon: @Home" ID="ID_635308207" CREATED="1427226636588" MODIFIED="1427226659054">
+<icon BUILTIN="gohome"/>
+</node>
+</node>
+<node TEXT="Test" POSITION="right" ID="ID_528722908" CREATED="1447323008857" MODIFIED="1447323239496">
+<edge COLOR="#00007c"/>
+<node TEXT="*@Meeing[Dogbert]Just{5minutes}@Hell#12" ID="ID_1976506990" CREATED="1447323014991" MODIFIED="1447323063711"/>
+<node TEXT="*#41@Meeting[Catbert]Just{5minutes}@Hell#2@Hell#42" ID="ID_1221496068" CREATED="1447323014991" MODIFIED="1447323141991"/>
+<node TEXT="*This@Context#1Will{today}be wrong" ID="ID_254804742" CREATED="1447323146784" MODIFIED="1447323217931"/>
+</node>
+</node>
+</map>
