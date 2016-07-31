7006f7b9fececa71daa9940df09be0aa565a5497
Gergely Papp <gergely.papp@itworks.hu>
2016-08-01 00:57:12 +0200

    Fixed docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,16 @@
-From mindweb/nodejs-base:latest
-USER node
-RUN mkdir config
+From node:latest
+
+
+USER root
+RUN useradd node
+RUN mkdir /home/node
+RUN mkdir /home/node/config
+RUN chown node /home/node
+
+WORKDIR /home/node
+ADD . /home/node
+RUN chown -R node /home/node
 EXPOSE 8081
+
+CMD []
+ENTRYPOINT ["/home/node/bin/startup.sh"]
--- docker_create.sh
+++ docker_create.sh
@@ -10,10 +10,12 @@ docker create \
     --env HTTP_PORT=${HTTP_PORT} \
     --env SERVER_PORT=${SERVER_PORT} \
     --volume `pwd`/../.config/$TYPE/server/config:/home/node/config \
+    --volume `pwd`/../.config/$TYPE/server/node_modules:/home/node/node_modules \
     --name $NAME \
     mindweb/server
 
 if [ ! -f  ../.config/$TYPE/server/config/config.json ]; then 
   mkdir -p ../.config/$TYPE/server/config
+  mkdir -p ../.config/$TYPE/server/node_modules
   cp config/config.json ../.config/$TYPE/server/config/config.json
 fi
